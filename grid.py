90371f1c540c39b42bf9beb28df3fb5aa03c9523
issamouu <issamouu69@gmail.com>
2019-11-04 23:05:36 +0100

    Add Grid class

---
+++ grid.py
@@ -0,0 +1,13 @@
+
+class Grid:
+
+    def __init__(self,number_of_columns,break_points,prefix='col',output_file='grid.css'):
+        self.number_of_columns = number_of_columns
+        self.break_points = break_points
+        self.output_file = output_file
+        self.prefix = prefix
+    
+    
+    
+
+                
